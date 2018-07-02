87a0b9db7ba5a7ebd5a8d04535c4da77de6ee89f
Artem <mrhard95@gmail.com>
2018-07-02 19:01:17 +0300

    API: questions destroy

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,4 +1,6 @@
 class API::QuestionsController < API::APIController
+  before_action :set_question, only: [:destroy]
+
   def index
     @questions = current_user.questions
   end
@@ -13,9 +15,18 @@ class API::QuestionsController < API::APIController
     end
   end
 
+  def destroy
+    @question.destroy
+    head :ok
+  end
+
   private
 
   def question_params
     params.require(:question).permit(:content, :respondent_id)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
