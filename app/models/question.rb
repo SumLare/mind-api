0cc1116c57ce103d34cb249a1863e3780877659b
Artem <mrhard95@gmail.com>
2018-07-06 00:15:36 +0300

    Validate max content length in question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,13 @@
 class Question < ApplicationRecord
+  MAX_CONTENT_LENGTH = 300
+
   belongs_to :user
   belongs_to :respondent, class_name: 'User'
 
   has_one :answer, dependent: :destroy
   has_many :upvotes, as: :upvotable, dependent: :destroy
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: MAX_CONTENT_LENGTH }
 
   def answered
     answer.present?
