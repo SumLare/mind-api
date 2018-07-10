fed9c0cf513a7a95eab0a2666be6389a8fb0e61b
Artem <mrhard95@gmail.com>
2018-07-10 09:48:23 +0300

    API: skip restrict access in search

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,4 +1,6 @@
 class API::SearchesController < API::APIController
+  skip_before_action :restrict_access!, only: [:show]
+
   def show
     @questions = Question.where('content ILIKE ?', "%#{params[:search]}%")
     @users = User.where("concat(first_name, ' ', last_name) ILIKE ?", "%#{params[:search]}%")
