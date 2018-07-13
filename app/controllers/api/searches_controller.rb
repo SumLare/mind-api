93bb0f6ccdfc740c5527351308e52fac5d8df191
Artem <mrhard95@gmail.com>
2018-07-13 10:13:29 +0300

    API: fix n + 1 in searches

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -2,7 +2,7 @@ class API::SearchesController < API::APIController
   skip_before_action :restrict_access!, only: [:show]
 
   def show
-    @questions = Question.where('content ILIKE ?', "%#{params[:search]}%")
-    @users = User.where("(first_name || ' ' || last_name) ILIKE ?", "%#{params[:search]}%")
+    @questions = Question.where('content ILIKE ?', "%#{params[:search]}%").includes(:answer, :upvotes)
+    @users = User.where("(first_name || ' ' || last_name) ILIKE ?", "%#{params[:search]}%").with_attached_avatar
   end
 end
--- app/views/api/searches/show.jbuilder
+++ app/views/api/searches/show.jbuilder
@@ -1,9 +1,7 @@
 json.questions @questions do |question|
   json.partial! 'api/questions/question', question: question
 
-  json.cache! question.answer do
-    json.partial! 'api/answers/answer', answer: question.answer
-  end
+  json.partial! 'api/answers/answer', answer: question.answer if question.answer
 end
 
 json.users @users do |user|
